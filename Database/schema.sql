f6d2f208ee88ba5c6a442c157b38f3cf75c08a73
Aaron Meadows <aaron.21@live.com>
2022-03-03 21:05:49 -0600

    Added tentative tables and relations

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,7 +1,44 @@
+-- Tables
 CREATE TABLE IF NOT EXISTS Users (
-    userID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    userName text NOT NULL,
-    userPassword text NOT NULL UNIQUE,
-    userEmail text NOT NULL,
-    userPhone int,
+    userID INTEGER PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    userName TEXT(20) NOT NULL,
+    userPassword TEXT(20) NOT NULL UNIQUE,
+    userEmail TEXT(20) NOT NULL,
+    userPhone INTEGER
 );
+
+CREATE TABLE IF NOT EXISTS Locations (
+    locationName TEXT(20) NOT NULL,
+    zip INTEGER NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    CONSTRAINT actualLocation PRIMARY KEY (locationName, zip)
+);
+
+CREATE TABLE IF NOT EXISTS Events (
+    eventName TEXT(20) NOT NULL,
+    eventDate TEXT(20) NOT NULL,
+    eventLocation TEXT(20) FOREIGN KEY REFERENCES Locations(actualLocation),
+    PRIMARY KEY (eventName, eventDate)
+);
+
+-- Relations
+
+CREATE TABLE UsersGoingTo (
+   userID INTEGER,
+   eventName  TEXT(30),
+   PRIMARY KEY (userID, eventName)
+  );
+
+CREATE TABLE FriendsWith (
+   userID INTEGER,
+   friendID  INTEGER,
+   PRIMARY KEY (userID, friendID)
+  );
+
+CREATE TABLE StrikedUsers (
+   userID INTEGER,
+   strikedID  INTEGER,
+   strikeNumber INTEGER,
+   PRIMARY KEY (userID, friendID)
+  );
