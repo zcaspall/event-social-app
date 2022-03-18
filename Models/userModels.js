ca6944732c0bfd60d8f302a471fadcb8bd83790f
Xavier <xaviercharlemagne728@gmail.com>
2022-03-17 23:24:22 -0500

    updating eventModels

--- Models/userModels.js
+++ Models/userModels.js
@@ -30,26 +30,4 @@ async function createUser(userName, userPassword, userEmail, userPhone){
     }
 };
 
-function createLocation(name, zip, lat, long){
-    const sql = `INSTERT INTO Locations
-                    (locationName, zip, latitude, longitude)
-                 VALUES
-                    (@name, @zip, @lat, @long)`;
-    
-    const stmt = db.prepare(sql);
-
-    try{
-        stmt.run({
-            "locationName": name,
-            "zip": zip,
-            "latitutde": lat,
-            "longitude": long,
-        });
-    }
-    catch(err){
-        console.error(err);
-    }
-};
-
-module.exports.createUser = createUser;
-module.exports.createLocation = createLocation;
+module.exports = createUser;
